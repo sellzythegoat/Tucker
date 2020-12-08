0a04a61268158bd090908821b18673f96fa5cee1
sellzythegoat <75702073+sellzythegoat@users.noreply.github.com>
2020-12-08 17:11:23 -0500

    Create Tucker.js

---
+++ Tucker.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <h1>Tucker The Savage Pup</h1>
+    <h3>How We Got Him</h3>
+    <p>We got Tucker in Connecticut when he was 8 weeks old. Now he is 5 months and he is growing so fast. The car ride to Connecticut from Massachusetts seemed like hours, but on the way back we were so entertained by the puppy so it felt like only a few minutes. The dog was climbing all over me and when I finally got him off, he just sat there and looked out the window. I was happy that we got him but I felt bad for taking him away from his siblings.</p>
+    <img src="puppy.jpg" alt="Puppy" width="270" height="170">
+    <h3>  The Baseball Fields </h3>
+    <p> We couldn't go straight home because my brother had a baseball game. While we were at the fields, Tucker met all of our friends and he was so exited! He took about twenty naps and tried to eat all of our shoes.</p>
+    <h6> Pixabay Free for commercial use
+      No attribution required</h6>
+
+    <h3> Home </h3>
+    <p>By the time we got home we were all tired but I think the most tired out of all of us was Tucker. We could hear fireworks in the distance and Tucker was a little bit scared. He already had a bunch of toys because we had been planning on getting him for months.</p>
+
+    <h3>Now</h3>
+    <p>Tucker is about eight months old now and he still loves food more than anything. He loves walks in the woods and playing fetch. The little dude runs away five times a day and eats like a horse, but we love him. He is the best dog that we could have ever wished for.</p>
+
+    <h5>This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</h>
+
+
+  </body>
+</html>
